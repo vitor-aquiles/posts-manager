1a4d5c348aeddd9b602de391e991f058836d46ed
Vitor Aquiles <aquilessrd@hotmail.com>
2019-12-28 09:43:59 -0300

    Implementing UserDTO

--- src/main/java/com/aquiles/postsmanager/resources/UserResource.java
+++ src/main/java/com/aquiles/postsmanager/resources/UserResource.java
@@ -1,15 +1,16 @@
 package com.aquiles.postsmanager.resources;
 
 import com.aquiles.postsmanager.domain.User;
+import com.aquiles.postsmanager.dto.UserDTO;
 import com.aquiles.postsmanager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,8 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> listUser = userService.findAll();
-        return ResponseEntity.ok().body(listUser);
+        List<UserDTO> listUserDTO = listUser.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listUserDTO);
     }
 }
